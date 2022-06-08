e30407144ff2fb8272e640c8eda88116a57e72c6
marcpetitvecino <marcpetitvecino@gmail.com>
2022-06-08 03:02:50 +0200

    Project finished

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+import { TodoItem } from './TodoItem'
+
+export function TodoList({todos}) {
+    return (
+        <ul>
+            {todos.map(todo => (
+                <TodoItem todo={todo}/>
+            ))}
+        </ul>
+    )
+}
